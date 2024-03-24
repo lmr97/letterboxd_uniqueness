22fff96fd2d14e3a7a20304657921377c2a080d6
Martin Reid <66138946+lmr97@users.noreply.github.com>
2024-03-23 23:42:27 -0600

    initial commit

---
+++ inc/letterboxdfinders.py
@@ -0,0 +1,39 @@
+import requests
+from selectolax.parser import HTMLParser
+
+# getting any set of information from the tabs 
+# in the center of the page on Letterboxd,
+# except for Releases; the logic there is different
+
+# attribute is a string, and can be any element you see in the tabs.
+# if there are multiple words in the attribute, it must have a hyphen (-)
+# between them. For example, 'assistant director' should be 'assistant-director'.
+def get_tabbed_attribute(url, attribute):
+    webpage = requests.get(url)
+    tree = HTMLParser(webpage.text)
+
+    # find HTML elements whose href contains 'attibute'
+    tabbed_elements = tree.css("a[href*='/" + attribute + "/']")
+
+    # extract text from found HTML elements
+    attribute_list = []
+    for element in tabbed_elements:
+        attribute_list.append(element.text())
+    
+    # return only distinct values, but still as a list
+    return set(attribute_list)
+
+
+def get_avg_rating(film_url):
+    webpage = requests.get(film_url)
+
+    tree = HTMLParser(webpage.text)
+    rating_element = tree.css("meta[name='twitter:data2']")   # returns list, albeit with 1 element
+    
+    avg_rating = None
+    if (len(rating_element) > 0):
+        rating_element_content = rating_element[0].attributes['content']
+        rating_element_title_parsed = rating_element_content.split(" ")
+        avg_rating = float(rating_element_title_parsed[0])
+
+    return avg_rating
---
+++ src/lbunique.py
@@ -0,0 +1,143 @@
+# this is the driver that calculates a metric that defines uniqueness, as follows:
+# 'uniqueness' is defined in terms of 'available deviance'.
+# 'available deviance' (AD) is the how much the rating could possibly differ from the average,
+# defined as max(avg_rating-0.5, avg_rating-5).
+# 'uniqueness' is the mean ratio of abs(user_rating-avg) to AD.
+
+
+import sys
+import numpy as np
+import pandas as pd
+import requests
+from selectolax.parser import HTMLParser
+import letterboxdfinders as lbf
+import math
+
+LB_HOME = "https://letterboxd.com/"
+OUTPUT_WIDTH = 50
+
+
+
+def print_loading_bar(rows_now, total_rows):
+    bar_width_now = math.ceil(OUTPUT_WIDTH * (rows_now+1)/total_rows)
+    print("| ", "█" * bar_width_now, 
+            (OUTPUT_WIDTH - bar_width_now) * " ", "|", 
+            f"{(rows_now+1)/total_rows:.0%}",
+            end = "\r")
+
+
+
+# gets the URLs to the films, as well as user ratings while the page was requested
+def get_user_ratings(username):
+    print("\nGetting user ratings...\n")
+    user_diary_url = LB_HOME + username + "/films/diary/"
+    diary_webpage = requests.get(user_diary_url)
+    diary_html = HTMLParser(diary_webpage.text)
+
+    # get last page
+    pagination_elements = diary_html.css("li.paginate-page")
+    last_page_num = int(pagination_elements[-1].text())
+
+
+    # get relevant nodes for URLs and user ratings
+    url_paths_list = []  # href's only have endings of urls to films
+    user_ratings_list = []
+    for page_num in range(last_page_num):
+        current_page = requests.get(user_diary_url + "page/" + str(page_num+1) + "/")
+        curr_page_html = HTMLParser(current_page.text)
+        url_paths_list += curr_page_html.css("div.poster")
+        user_ratings_list += curr_page_html.css("span.rating")
+
+
+    # unpack URLs from div nodes
+    for i, divNode in enumerate(url_paths_list):
+        # the 'data-film-slug' attribute is the unique key for the film
+        url_paths_list[i] = LB_HOME + "film/" + divNode.attributes["data-film-slug"] + "/"
+
+    
+    # user ratings are rendered as strings of star characters,
+    # need to parse the list to convert to numbers
+    star_string = ""
+    full_stars = 0
+    half_star = 0
+    for i, rating in enumerate(user_ratings_list):
+        star_string = rating.text()
+        full_stars = star_string.count('★')
+        half_star  = star_string.count('½') * 0.5
+        user_ratings_list[i] = full_stars + half_star
+
+
+    urls_user_ratings_dict = {
+        'Film URL': url_paths_list,
+        'User Rating': user_ratings_list
+    }
+    user_ratings_df = pd.DataFrame(urls_user_ratings_dict)
+
+    return user_ratings_df
+    
+
+
+# this gets the site-wide average film rating
+def get_avg_rating_col(url_col):
+    print("Getting average ratings...")
+
+    total_films = url_col.size
+    np_col = np.empty((total_films,1), dtype=float)
+
+    for i, url in enumerate(url_col):
+        print_loading_bar(i, total_films)
+        np_col[i] = lbf.get_avg_rating(url)
+    
+    print()  # to put carriage on new line after loading bar function
+    avg_ratings_df = pd.DataFrame(np_col, columns=['Average Rating'])
+    return avg_ratings_df
+
+
+# is the average closer to the low end or the high end?
+def get_available_dev(average_rating):
+    return max(average_rating-0.5, 5-average_rating)
+
+
+# 'uniqueness' is defined in terms of 'available deviance'.
+# 'available deviance' (AD) is the how much the rating could possibly differ from the average,
+# defined as max(avg_rating-0.5, avg_rating-5).
+# 'uniqueness' is the mean ratio of abs(user_rating-avg) to AD.
+def calc_uniqueness(ratings_only):
+    print("\nCalculating uniqueness...")
+
+    ratings_only['Absolute Difference'] = abs(ratings_only['Average Rating'] - ratings_only['User Rating'])
+    ratings_only['Available Deviance']  = ratings_only['Average Rating'].apply(get_available_dev)
+    ratings_only['Uniqueness'] = ratings_only['Absolute Difference'] / ratings_only['Available Deviance']
+
+    return ratings_only['Uniqueness'].mean()
+
+
+def is_valid_user(username):
+    print("\nValidating username...")
+    
+    userpage = requests.get(LB_HOME+username)
+    userpage_html = HTMLParser(userpage.text)
+
+    # get last page
+    error_elements = userpage_html.css("body.error")
+
+    return not bool(len(error_elements))
+
+
+def main():
+    user = sys.argv[1]
+
+    if (is_valid_user(user)):
+        ratings_df = get_user_ratings(user)  # starts with the URLs and user ratings
+        ratings_df = ratings_df.join(get_avg_rating_col(ratings_df['Film URL']))  # get average ratings
+
+        uniqueness = calc_uniqueness(ratings_df[['User Rating', 'Average Rating']])
+        print(f"Uniqueness score for {user}: {round(uniqueness,4)}")
+    else:
+        print("\nInvalid username. Try again with a different username.\n")
+
+
+
+
+if (__name__ == "__main__"):
+    main()
